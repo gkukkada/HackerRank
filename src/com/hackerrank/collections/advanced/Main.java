0ad3665a6771eb98d7c71d5b3d605806bfca2ebc
Kukkadapu <AAB2873@carefirst.com>
2016-10-05 15:59:40 -0400

    added singleton and instances challenges.

---
+++ src/com/hackerrank/collections/advanced/Main.java
@@ -0,0 +1,45 @@
+package com.hackerrank.collections.advanced;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/*
+ * https://www.hackerrank.com/challenges/java-iterator
+*/
+public class Main {
+
+	static Iterator func(ArrayList mylist) {
+		Iterator it = mylist.iterator();
+		while (it.hasNext()) {
+			Object element = it.next();
+			if (element instanceof String) {// Hints: use instanceof operator
+				break;
+			}
+		}
+		return it;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList mylist = new ArrayList();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			mylist.add(sc.nextInt());
+		}
+
+		mylist.add("###");
+		for (int i = 0; i < m; i++) {
+			mylist.add(sc.next());
+		}
+
+		Iterator it = func(mylist);
+		while (it.hasNext()) {
+			Object element = it.next();
+			System.out.println((String) element);
+		}
+	}
+}
---
+++ src/com/hackerrank/collections/advanced/SingletonClass.java
@@ -0,0 +1,20 @@
+package com.hackerrank.collections.advanced;
+
+/*
+ * https://www.hackerrank.com/challenges/java-singleton
+ */
+class Singleton {
+	public String str;
+	private static Singleton instance = null;
+
+	protected Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		if (instance == null) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+
+}
